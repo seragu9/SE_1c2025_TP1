5a97a82da39b4c777e84ada6c7f2012bac3a7b18
seragu9 <50808352+seragu9@users.noreply.github.com>
2025-05-02 21:35:40 +0000

    Codigo de TP2
    Entrega completa

---
+++ button/button.h
@@ -0,0 +1,17 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _BUTTON_H_
+#define _BUTTON_H_
+
+//=====[Declaration of public defines]=========================================
+#define TIME_INCREMENT_MS 10
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+
+void debounceButtonInit();
+bool debounceButtonUpdate();
+//=====[#include guards - end]=================================================
+
+#endif // _BUTTON_H_
---
+++ serial_com/serial_com.h
@@ -0,0 +1,17 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _SERIAL_COM_H_
+#define _SERIAL_COM_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+
+void sendUartMessage(const char * tosend);
+
+//=====[#include guards - end]=================================================
+
+#endif // _SERIAL_COM_H_
